5d67fd12514f94cd1c776e451e30ec16fa8a4129
Duc Huy <duchuy.ecng@gmail.com>
2021-05-25 15:22:04 +0700

    Initial Commit

---
+++ IOS_Practice_TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS_Practice_TableView
+//
+//  Created by Nguyen Duc Huy on 5/25/21.
+//  Copyright © 2021 Nguyen Duc Huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
